f52b32ca78c9b2f829eb85e02d80b8fecc398e53
Mohd Shibli Qureshi <mohdshibli27@gmail.com>
2019-10-05 19:56:28 +0530

    depenedencies added

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='kyubi',
       version='0.1.0',
       packages=['kyubi'],
+      install_requires=['colorterm', 'requests', 'colorama', 'pyfiglet', 'argparse'],
       entry_points={
           'console_scripts': [
               'kyubi = kyubi.core:main'
