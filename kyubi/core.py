28682359bf5fe80f94f7bf89844b00f3a955d6e0
Mohd Shibli Qureshi <mohdshibli27@gmail.com>
2019-10-05 19:52:55 +0530

    verbosity enabled

--- kyubi/core.py
+++ kyubi/core.py
@@ -13,7 +13,7 @@ init()
 
 argparser = ag.ArgumentParser(description="This is a nginx traversal tool")
 argparser.add_argument("url", type=str, help="URL of the target")
-argparser.add_argument("-v", help="increase verbosity", action="store_true", dest="verbose")
+argparser.add_argument("-v", help="increase verbosity", action="store_true")
 argparser.add_argument("-a", help="append segment in the end", action="store_true")
 
 args = argparser.parse_args()
@@ -22,11 +22,12 @@ valid = ['403', '400', '200']
 payloads = ["../", "../../", "../../../../../../../../../../../"]
 
 def make_a_request (url):
+    resp = 500
     try:
-        resp = rq.get(url, verify=True)
+        resp = rq.get(url, verify=True).status_code
     except Exception as e:
-        resp.status_code = 500
-    return str(resp.status_code)
+        resp = 500
+    return str(resp)
 
 def main():
     parser = urlparse(args.url)
@@ -36,7 +37,7 @@ def main():
     segments = segments[1: len(segments)]
 
     _str = ""
-    _marks = ('-'*80)
+    _marks = ('-' * 80)
     sys.stdout.write("\n{0:50}\t{1}\n{2}\n".format("URL", "Status",_marks))
     for segment in segments:
         _str += "/"+segment
@@ -46,14 +47,20 @@ def main():
         for payload in payloads:
             _url = "{}://{}{}{}{}".format(parser.scheme, parser.netloc, _str, payload, _x)
             statcode = make_a_request(_url)
-            sys.stdout.write("{0} [{1}]\n".format(_url, colored(statcode, 'green') if statcode in valid else colored(statcode, 'red')))
-
+            if(args.v):
+                sys.stdout.write("{0} [{1}]\n".format(_url, colored(statcode, 'green') if statcode in valid else colored(statcode, 'red')))
+            else:
+                if(statcode in valid):
+                    sys.stdout.write("{0} [{1}]\n".format(_url, colored(statcode, 'green')))
     for i in range(0, len(segments)-1):
         for payload in payloads:
             _url = parser.scheme+"://"+parser.netloc+"/" + '/'.join(segments[0:i+1]) + payload + '/'.join(segments[i+1:len(segments)])
             statcode = make_a_request(_url)
-            sys.stdout.write("{} [{}]\n".format(_url, colored(statcode, 'green') if statcode in valid else colored(statcode, 'red')))
-
+            if(args.v):
+                sys.stdout.write("{} [{}]\n".format(_url, colored(statcode, 'green') if statcode in valid else colored(statcode, 'red')))
+            else:
+                if(statcode in valid):
+                    sys.stdout.write("{0} [{1}]\n".format(_url, colored(statcode, 'green')))
 
 
 
